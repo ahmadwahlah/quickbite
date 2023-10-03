6e7d1c911367062b29d51d98ebe4d433d45bac2f
ahmadwahlah <ahmadwahlah@gmail.com>
2023-10-03 23:19:02 +0500

    footer component updated

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <footer className="d-flex flex-wrap justify-content-center align-items-center py-3 my-4 border-top">
+        <div className="col-md-4 d-flex align-items-center justify-content-center">
+          <span className="mb-3 mb-md-0 text-muted">© 2023 QuickBite, Inc</span>
+        </div>
+      </footer>
+    </div>
+  );
 }
